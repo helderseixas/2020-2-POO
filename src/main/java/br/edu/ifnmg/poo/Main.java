ad458c4e6d5684413c6409b8a18f9983126e0ec8
helderseixas <helderseixas@gmail.com>
2021-02-05 09:02:33 -0300

    Modificações feitas durante a aula do dia 05/02/2021.

--- src/main/java/br/edu/ifnmg/poo/Main.java
+++ src/main/java/br/edu/ifnmg/poo/Main.java
@@ -6,6 +6,7 @@
 package br.edu.ifnmg.poo;
 
 import br.edu.ifnmg.poo.exemploveiculo.Veiculo;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,23 +18,36 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {        
+    public static void main(String[] args) {    
+        //Veiculo[] veiculos = new Veiculo[3];
+        ArrayList<Veiculo> veiculos = new ArrayList<>();
+        
         //Declaracão de uma referência
         Veiculo uno;
         //Instaciacão de um objeto e atribuicão à uma referência
-        uno = new Veiculo("Carro", "Uno Mile", "Fiat", 2010, 100000);                        
-        //Acessar método
-        uno.deslocar(100);
-        imprimirVeiculo(uno);
+        uno = new Veiculo("Carro", "Uno Mile", "Fiat", 2010, 100000, "João");        
+        //Acessar método        
+        uno.deslocar(100, "José");
+        //veiculos[0] = uno;    
+        veiculos.add(uno);
+        
         
         Veiculo ka;
-        ka = new Veiculo("Carro", "Ka", "Ford", 2020, 0);                
+        ka = new Veiculo("Carro", "Ka", "Ford", 2020); 
+        ka.setMotorista("Maria");
         ka.deslocar(10);
         ka.deslocar(-5);
-                
-        imprimirVeiculo(ka);
+        //veiculos[1] = ka;  
+        veiculos.add(ka);
+        
+        Veiculo gol;
+        gol = new Veiculo("Carro", "Gol", "VW", 2021);
+        //veiculos[2] = gol;
+        veiculos.add(gol);
         
         System.out.printf("\nTotal veículos: %d", Veiculo.getTotalVeiculos());
+        
+        imprimirVeiculo(veiculos);
     }
     
     static void imprimirVeiculo(Veiculo veiculo){
@@ -42,5 +56,26 @@ public class Main {
         System.out.printf("\nMarca: %s", veiculo.getMarca());
         System.out.printf("\nAno: %d", veiculo.getAno());
         System.out.printf("\nKilometragem: %.2f", veiculo.getKilometragem());
+        System.out.printf("\nMotorista: %s", veiculo.getMotorista());
     }    
+    
+    static void imprimirVeiculo(Veiculo veiculos[]){
+//        for(Veiculo veiculo : veiculos){
+//            imprimirVeiculo(veiculo);
+//        }
+        for(int i = 0; i < veiculos.length; i++){
+            Veiculo veiculo = veiculos[i];
+            imprimirVeiculo(veiculo);
+        }
+    }
+    
+    static void imprimirVeiculo(ArrayList<Veiculo> veiculos){
+//        for(Veiculo veiculo : veiculos){
+//            imprimirVeiculo(veiculo);
+//        }
+        for(int i = 0; i < veiculos.size(); i++){
+            Veiculo veiculo = veiculos.get(i);
+            imprimirVeiculo(veiculo);
+        }
+    }
 }
--- src/main/java/br/edu/ifnmg/poo/exemploveiculo/Veiculo.java
+++ src/main/java/br/edu/ifnmg/poo/exemploveiculo/Veiculo.java
@@ -18,17 +18,27 @@ public class Veiculo {
     private String marca;
     private int ano;
     private double kilometragem;
+    private String motorista;
     
     public Veiculo(String tipo, String modelo, String marca, int ano, double kilometragem){
+        this(tipo, modelo, marca, ano, kilometragem, null);
+    }
+    
+    public Veiculo(String tipo, String modelo, String marca, int ano){
+        this(tipo, modelo, marca, ano, 0);
+    }
+    
+    public Veiculo(String tipo, String modelo, String marca, int ano, double kilometragem, String motorista){
         this.tipo = tipo;
         this.modelo = modelo;
         this.marca = marca;
-        this.ano = ano;
+        this.ano = ano;        
         if(kilometragem > 0){
             this.kilometragem = kilometragem;
         }else{
             this.kilometragem = 0;
         }
+        this.motorista = motorista;
         
         totalVeiculos++;
     }
@@ -38,6 +48,11 @@ public class Veiculo {
         if(kilometragemDeslocamento > 0){
             this.kilometragem += kilometragemDeslocamento;
         }
+    }
+    
+    public void deslocar(double kilometragemDeslocamento, String motorista){
+        this.motorista = motorista;
+        this.deslocar(kilometragemDeslocamento);
     }
     
     public static int getTotalVeiculos(){
@@ -63,4 +78,12 @@ public class Veiculo {
     public double getKilometragem() {
         return kilometragem;
     }    
+    
+    public void setMotorista(String motorista){
+        this.motorista = motorista;
+    }
+    
+    public String getMotorista(){
+        return this.motorista;
+    }
 }
