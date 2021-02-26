7782d8f9c2eb3f47bbc9d904327bbf5165ae649b
helderseixas <helderseixas@gmail.com>
2021-02-26 09:12:54 -0300

    Modificações feitas durante a aula do dia 26/02/2021: implementacão das classes do modelo. Correcão da modelagem de classes e criacão do diagrama de pacotes.

---
+++ src/main/java/br/edu/ifnmg/poo/banco/modelo/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.poo.banco.modelo;
+
+/**
+ *
+ * @author helder
+ */
+public abstract class Cliente {
+    private String nome;
+    private String senha;
+    
+}
